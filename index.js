eb0190a5aaa190f147be876271ebb86159c57ade
angie <angieangerilli@gmail.com>
2019-02-15 19:34:52 -0500

    forLoop

---
+++ index.js
@@ -0,0 +1,16 @@
+
+//logs 1-20
+
+for (i=1; i<=20; i++){
+  console.log(i);
+}
+
+//logs even numbers
+for(i=2; i<=20; i+=2){
+    console.log(i);
+  }
+
+//logs odd numbers
+for(i=1; i<=20; i+=2){
+    console.log(i);
+  }
